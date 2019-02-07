a1b23ca54a9b73c2be2ed588284b3a9d4a0581ca
NicolasGensollen <nicolas.gensollen@gmail.com>
2019-02-07 17:29:12 +0100

    Add empty module

---
+++ src/StreamGraphs.jl
@@ -0,0 +1,17 @@
+
+"""
+Module for studying stream graphs and link streams.
+"""
+
+module StreamGraphs
+
+########################################
+# Exports
+
+#######################################
+# Imports
+
+#######################################
+# Includes
+
+end
