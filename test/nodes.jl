9e963f47f641e484140ef6075e2767e3c7642373
NicolasGensollen <nicolas.gensollen@gmail.com>
2019-02-13 12:51:45 +0100

    Add tests for operations on arrays of dynamic nodes

--- test/nodes.jl
+++ test/nodes.jl
@@ -48,20 +48,45 @@ c = DNode("c",Intervals([(0.000001,0.99)]))
 @test (a ∩ c).list == [(0.000001,0.99)]
 @test (a ∪ c).list == [(0.0,2.0)]
 
-l1 = [a,c,b]
-l2 = [a,c,b]
-l3 = [b,c,a]
-l4 = [c,a,b]
-l5 = [a]
-l6 = [c,b,a]
-
-@test l1==l2
-@test l1==l1
-@test l1==l3
-@test l3==l2
-@test l1!=l4
-@test l4!=l2
-@test l4 ⊆ l1
-@test l5 ⊆ l6
-@test l6 ⊈ l5
-@test l5 != l6
+a = DNode("a",Intervals([(0.0,1.0),(1.0,2.0)]))
+b = DNode("b",Intervals([(0.0,1.5),(1.6,2.5)]))
+c = DNode("c",Intervals([(1.0,1.6)]))
+d = DNode("d",Intervals([(0.3,1.2)]))
+L = [a,b,c]
+@test a ∈ L
+@test b ∈ L
+@test d ∉ L
+@test c ∈ L
+@test get_idx(a,L) == [1]
+@test get_idx(b,L) == [2]
+@test get_idx(d,L) == []
+@test get_idx("a",L) == [1]
+@test get_idx("b",L) == [2]
+@test get_idx("d",L) == []
+@test get_idx(1.0,L) == [1,2,3]
+@test get_idx(1.61,L) == [1,2]
+@test get_idx(2.4,L) == [2]
+@test get_idx(2.6,L) == []
+@test a ⊆ L
+@test d ⊈ L
+@test DNode("a",Intervals([(0.3,0.9)])) ⊆ L
+@test DNode("a",Intervals([(0.3,1.2)])) ⊆ L
+@test DNode("b",Intervals([(1.3,1.5)])) ⊆ L
+@test DNode("b",Intervals([(1.3,1.9)])) ⊈ L
+L2 = [a,c]
+@test L2 ⊆ L
+@test L ∩ L2 == [a,c]
+@test L ∪ L2 == [a,b,c]
+L2 = [a,b,d]
+@test L2 ⊈ L
+@test L ∩ L2 == [a,b]
+@test L ∪ L2 == [a,b,c,d]
+@test L2 ∪ L == [a,b,d,c]
+L2 = []
+@test L2 ⊆ L
+@test L ∩ L2 == []
+@test L ∪ L2 == L
+L2 = [DNode("a",Intervals([(0.3,0.9)])), DNode("b",Intervals([(1.3,1.5)]))]
+@test L2 ⊆ L
+L2 = [DNode("a",Intervals([(0.3,0.9)])), DNode("b",Intervals([(1.3,1.9)]))]
+@test L2 ⊈ L
