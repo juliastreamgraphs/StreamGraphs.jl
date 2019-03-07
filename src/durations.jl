bf6657d193d253f26103d4566eca0f353aad8bc8
NicolasGensollen <nicolas.gensollen@gmail.com>
2019-03-07 12:20:46 +0100

    Overload node and link durations with TimeCursor

--- src/durations.jl
+++ src/durations.jl
@@ -6,9 +6,37 @@ duration(W::Dict{AbstractString,Node})=sum([duration(v) for (k,v) in W])
 
 node_duration(ls::Union{LinkStream,DirectedLinkStream})=duration(ls)
 
-node_duration(s::Union{StreamGraph,DirectedStreamGraph})=length(s.V)>0 ? sum([duration(n) for (k,n) in s.W])/length(s.V) : 0
+function node_duration(s::Union{StreamGraph,DirectedStreamGraph})
+	length(s.V)>0 ? sum([duration(n) for (k,n) in s.W])/length(s.V) : 0
+end
 
-link_duration(s::AbstractStream)=length(s.V)>1 ? 2*sum([duration(l) for (k,v) in s.E for (kk,l) in v])/(length(s.V)*(length(s.V)-1)) : 0
+function node_duration(s::Union{StreamGraph,DirectedStreamGraph},tc::TimeCursor)
+	k::Float64=0.0
+	length(s.V)==0 && return 0.0
+	start!(tc)
+	k+=duration(tc.S)*number_of_nodes(tc)
+	while tc.S.t1 < s.T.list[1][2]
+		next!(tc)
+		k+=duration(tc.S)*number_of_nodes(tc)
+	end
+	k/length(s.V)
+end
+
+function link_duration(s::AbstractStream)
+	length(s.V)>1 ? 2*sum([duration(l) for (k,v) in s.E for (kk,l) in v])/(length(s.V)*(length(s.V)-1)) : 0
+end
+
+function link_duration(s::AbstractStream,tc::TimeCursor)
+	k::Float64=0.0
+	length(s.V)<=1 && return 0.0
+	start!(tc)
+	k+=duration(tc.S)*number_of_links(tc)
+	while tc.S.t1 < s.T.list[1][2]
+		next!(tc)
+		k+=duration(tc.S)*number_of_links(tc)
+	end
+	k/(length(s.V)*(length(s.V)-1))
+end
 
 duration(s::State)=s.t1-s.t0
 
